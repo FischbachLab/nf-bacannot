96a2051514f8d97aee0c666c302a791e3d676213
Sunit Jain <mail4sunit@gmail.com>
2022-11-14 11:47:33 -0800

    more universal renaming

--- bin/renameFastaHeaders.py
+++ bin/renameFastaHeaders.py
@@ -5,8 +5,11 @@ from Bio import SeqIO
 input_fasta = sys.argv[1]
 output_fasta = sys.argv[2]
 
+sequence_number  = 0
+
 with open(output_fasta, "w") as outputs:
     for r in SeqIO.parse(input_fasta, "fasta"):
+        sequence_number += 1
         r.description = r.id
-        r.id = "_".join((r.description).split("_")[1:])
+        r.id = f"Node_{sequence_number}"
         SeqIO.write(r, outputs, "fasta")
