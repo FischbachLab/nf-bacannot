08e817daacddf1dffe84e10a6b6829ff5b082735
Sunit Jain <mail4sunit@gmail.com>
2022-12-19 12:52:04 -0800

    updated to use bakta profile

--- bin/createSubmissionYaml.py
+++ bin/createSubmissionYaml.py
@@ -1,43 +1,102 @@
 #!/usr/bin/env python3
 """
 USAGE:
-python3 createSubmissionYaml.py \\
-    renamed \\
-    s3://genomics-workflow-core/Results/Bacannot/hCom2/20221102/inputs \\
-    hCom2.yaml
+python3 createSubmissionYaml.py \
+    -g s3://maf-users/Nathan_Johns/Scratch/ \
+    -project UHGG_Annotation \
+    -prefix 20221219-test-1 \
+    -s test.yaml \
+    -b
 
 Whats happening:
 
-- Genomes from "renamed" folder are being uploaded to the s3path/"fasta" folder.
-- A hCom2.yaml file is created, based on specs in the baccannot pipeline and uploaded to the s3path folder.
+- Genomes from "s3://maf-users/Nathan_Johns/Scratch/" folder are being uploaded to the s3_output_path/"fasta" folder.
+- A test.yaml file is created, based on specs in the baccannot pipeline and uploaded to the s3_output_path folder.
 - The script generates a recommended command that you should use to submit the job.
 
 Output:
-aws batch submit-job \\
-    --job-name nf-bacannot-hCom2 \\
-    --job-queue priority-maf-pipelines \\
-    --job-definition nextflow-production \\
-    --container-overrides command=FischbachLab/nf-bacannot,\\
-"-profile","maf",\\
-"--input","s3://genomics-workflow-core/Results/Bacannot/hCom2/20221102/inputs/hCom2.yaml",\\
-"--output","s3://genomics-workflow-core/Results/Bacannot/hCom2/20221102"
+aws batch submit-job \
+    --job-name nf-bacannot-20221219-test-1 \
+    --job-queue priority-maf-pipelines \
+    --job-definition nextflow-production \
+    --container-overrides command=FischbachLab/nf-bacannot,\
+"-profile",maf_bakta,\
+"--input","s3://genomics-workflow-core/Results/Bacannot/UHGG_Annotation/20221219-test-1/inputs/test.yaml",\
+"--output","s3://genomics-workflow-core/Results/Bacannot/UHGG_Annotation/20221219-test-1"
 """
 
-import sys
 from cloudpathlib import CloudPath, AnyPath
 
-# import yaml
+import argparse
 from ruamel.yaml import YAML
-from pprint import pprint
+
+RESULTS_S3_BASE_PATH = CloudPath("s3://genomics-workflow-core/Results/Bacannot")
+
+parser = argparse.ArgumentParser(
+    description="Create submission YAML file for bacannot pipeline"
+)
+parser.add_argument(
+    "-g",
+    "--genome-dir",
+    type=str,
+    required=True,
+    help="Directory with all the genomes with extension '.fna'",
+)
+parser.add_argument(
+    "-project",
+    type=str,
+    required=False,
+    default="00_TEST",
+    help=(
+        """
+    Name of the project that this analysis belongs to.
+    This will become part of your output path.
+    No spaces, please.
+    """
+    ),
+)
+parser.add_argument(
+    "-prefix",
+    type=str,
+    required=False,
+    default="00_test_prefix",
+    help=(
+        """
+    Name of the subset of the data within this Project that this analysis belongs to.
+    This will become part of your output path.
+    No spaces, please.
+    """
+    ),
+)
+parser.add_argument(
+    "-b",
+    "--use-bakta",
+    action="store_true",
+    help="Use 'Bakta' to annotate the genomes.",
+)
+parser.add_argument(
+    "-s",
+    "--seedfile",
+    type=str,
+    required=False,
+    help="Name of the seedfile for the pipeline. By default, it is a combination of <PROJECT> and <PREFIX>",
+)
+
+args = parser.parse_args()
 
 yaml = YAML()
 yaml.default_flow_style = False
 yaml.sort_keys = False
 yaml.indent(mapping=2, sequence=4, offset=2)
 
-local_fasta_dir = AnyPath(sys.argv[1])
-s3_output_dir = CloudPath(sys.argv[2])
-samplesheet_yaml = AnyPath(sys.argv[3])
+fasta_dir = AnyPath(args.genome_dir)
+s3_output_dir = RESULTS_S3_BASE_PATH / args.project / args.prefix / "inputs"
+
+samplesheet_yaml = (
+    AnyPath(args.seedfile)
+    if args.seedfile
+    else AnyPath(f"{args.project}-{args.prefix}.yaml")
+)
 
 s3_fasta_dir = s3_output_dir / "fasta"
 s3_yaml_path = s3_output_dir / samplesheet_yaml.name
@@ -45,12 +104,12 @@ s3_yaml_path = s3_output_dir / samplesheet_yaml.name
 yaml_dict = {"samplesheet": []}
 
 # Upload genomes to S3
-for local_fasta_file in local_fasta_dir.glob("*.fna"):
-    s3path = s3_fasta_dir / local_fasta_file.name
+for fasta_file in fasta_dir.glob("*.fna"):
+    s3path = s3_fasta_dir / fasta_file.name
     yaml_dict["samplesheet"].append(
-        {"id": str(local_fasta_file.stem), "assembly": str(s3path)}
+        {"id": str(fasta_file.stem), "assembly": str(s3path)}
     )
-    s3path.upload_from(local_fasta_file)
+    s3path.upload_from(fasta_file)
 
 # Write YAML file to S3
 with s3_yaml_path.open("w") as s3_yaml_file:
@@ -59,15 +118,17 @@ with s3_yaml_path.open("w") as s3_yaml_file:
 # with open(samplesheet_yaml, "w") as s3_yaml_file:
 #     yaml.dump(yaml_dict, s3_yaml_file)
 
+launch_profile = "maf_bakta" if args.use_bakta else "maf"
+
 print(
     f"""
 Submission command:\n
 aws batch submit-job \\
-    --job-name nf-bacannot-{samplesheet_yaml.stem} \\
+    --job-name nf-bacannot-{args.prefix} \\
     --job-queue priority-maf-pipelines \\
     --job-definition nextflow-production \\
     --container-overrides command=FischbachLab/nf-bacannot,\\
-"-profile","maf",\\
+"-profile",{launch_profile},\\
 "--input","{s3_yaml_path}",\\
 "--output","{s3_output_dir.parent}"
 """
